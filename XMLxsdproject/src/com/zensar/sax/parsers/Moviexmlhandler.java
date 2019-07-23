c190b6ee15cb9d5b3d707554d0c77688c04687a8
Swarima15 <swarimasharma@gmail.com>
2019-07-22 18:01:56 -0700

    Modified xmlxsd

--- XMLxsdproject/src/com/zensar/sax/parsers/Moviexmlhandler.java
+++ XMLxsdproject/src/com/zensar/sax/parsers/Moviexmlhandler.java
@@ -9,25 +9,28 @@ public class Moviexmlhandler extends DefaultHandler {
 	@Override
 	public void startDocument() throws SAXException {
 		// TODO Auto-generated method stub
-		super.startDocument();
+		System.out.println("Starting xml Document");
 	}
 
 	@Override
 	public void endDocument() throws SAXException {
 		// TODO Auto-generated method stub
-		super.endDocument();
+		System.out.println("End of xml Document");
+	
 	}
 
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
 		// TODO Auto-generated method stub
-		super.startElement(uri, localName, qName, attributes);
+		System.out.println("Start element" + qName);
+
 	}
 
 	@Override
 	public void endElement(String uri, String localName, String qName) throws SAXException {
 		// TODO Auto-generated method stub
-		super.endElement(uri, localName, qName);
+		System.out.println("End element" + qName);
+
 	}
 
 	@Override
